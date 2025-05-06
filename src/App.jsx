3fbd4193b20cd86613b391595cfe91f12cdff418
NitijKuwar <nitijkuwar555@gmail.com>
2025-05-06 14:54:42 +0545

    removed build script

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import About from "./pages/About";
 import Cart from "./components/Cart";
 import { CartProvider } from "./pages/cartContext";
 import { ToastContainer } from "react-toastify";
+import SignUp from "./pages/SignUp";
+import LogIn from "./pages/LogIn";
 
 function App() {
   return (
@@ -16,8 +18,11 @@ function App() {
           <ToastContainer position="top-right" />
           <Routes>
             <Route path="/contact" element={<Contact />} />
+
             <Route path="/about" element={<About />} />
             <Route path="/viewcart" element={<Cart />} />
+            <Route path="/login" element={<LogIn />} />
+            <Route path="/signup" element={<SignUp />} />
           </Routes>
         </BrowserRouter>
       </CartProvider>
---
+++ src/pages/LogIn.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const LogIn = () => {
+  return (
+    <>
+      <form action="">
+        <input
+          type="email"
+          name=""
+          id=""
+          placeholder="Username"
+          className="border black"
+        />
+        <input type="password" name="" id="" placeholder="Password" />
+      </form>
+    </>
+  );
+};
+
+export default LogIn;
